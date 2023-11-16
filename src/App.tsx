06646fb5df735a23d71959597ffaa560cea741bb
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-11-16 23:18:14 +0700

    Remove signature image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import './style.css'
-import signature from './signature.png'
-import logo from './vercel.svg'
 import ClickCounter from './ClickCounter'
+import './style.css'
+import logo from './vercel.svg'
 const App = () => {
   return (
     <>
       <h1>
         Hello App - {process.env.NODE_ENV} - {process.env.name}
       </h1>
-      <img src={signature} alt="signature" width={300} height={100} />
       <img src={logo} alt="logo" width={100} height={100} />
       <ClickCounter />
     </>
